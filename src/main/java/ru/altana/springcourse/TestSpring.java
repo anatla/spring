959d5202a30028bfae2f7bed08e149381bd91388
anatla <aaa@labis.su>
2023-12-06 15:20:35 +0900

    9th lesson

--- src/main/java/ru/altana/springcourse/TestSpring.java
+++ src/main/java/ru/altana/springcourse/TestSpring.java
@@ -8,30 +8,17 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        Music music = context.getBean("rockMusic", Music.class);
 
-        System.out.println(classicalMusic.getSong());
+        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        // Music music = context.getBean("musicBean", Music.class);
-        // MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
 
-//        MusicPlayer firstMusicPlayer = context.getBean( "musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondMusicPlayer = context.getBean( "musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-//
-//        System.out.println(comparison);
-//
-//        System.out.println(firstMusicPlayer);
-//        System.out.println(secondMusicPlayer);
-//
-//        firstMusicPlayer.setVolume(10);
-//
-//        System.out.println(firstMusicPlayer.getVolume());
-//        System.out.println(secondMusicPlayer.getVolume());
+        Music music2 = context.getBean("classicalMusic", Music.class);
 
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+
+        classicalMusicPlayer.playMusic();
 
         context.close();
     }
