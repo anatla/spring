f750f75fa69c3a13b3baeb628578575c5130634d
anatla <aaa@labis.su>
2023-12-06 15:08:06 +0900

    7-8 lessons

--- src/main/java/ru/altana/springcourse/TestSpring.java
+++ src/main/java/ru/altana/springcourse/TestSpring.java
@@ -8,15 +8,30 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
         // Music music = context.getBean("musicBean", Music.class);
         // MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean( "musicPlayer", MusicPlayer.class);
+//        MusicPlayer firstMusicPlayer = context.getBean( "musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean( "musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
-        musicPlayer.playMusic();
-
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
