0ce835db8ef67fc7bf5cee5c52cad6b85a3f377e
anatla <aaa@labis.su>
2023-12-15 16:39:19 +0900

    10th lesson

--- src/main/java/ru/altana/springcourse/MusicPlayer.java
+++ src/main/java/ru/altana/springcourse/MusicPlayer.java
@@ -1,15 +1,20 @@
 package ru.altana.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 }
