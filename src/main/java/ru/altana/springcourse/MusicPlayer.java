9c15d98e1cf434cf17d1c016da8aa5712efe3d08
anatla <altana.alexeeva@gmail.com>
2023-12-22 00:09:06 +0900

    13th lesson

--- src/main/java/ru/altana/springcourse/MusicPlayer.java
+++ src/main/java/ru/altana/springcourse/MusicPlayer.java
@@ -1,11 +1,8 @@
 package ru.altana.springcourse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class MusicPlayer {
 
     @Value("${musicPlayer.name}")
